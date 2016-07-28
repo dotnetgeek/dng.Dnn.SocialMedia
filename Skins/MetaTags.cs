5289618fd718091b1566443babf2834f9aa79926
Daniel Müller <daniel@dotnetgeek.com>
2016-07-29 00:25:43 +0200

    First simple version

---
+++ Skins/MetaTags.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Web.UI.HtmlControls;
+using DotNetNuke.UI.Skins;
+
+namespace dng.Dnn.SocialMedia.Skins
+{
+    public class MetaTags : SkinObjectBase
+    {
+        #region Public Constructors
+
+        public MetaTags()
+        {
+            EnableFacebook = true;
+            EnableTwitter = true;
+        }
+
+        #endregion Public Constructors
+
+        #region Public Properties
+
+        public bool EnableFacebook { get; set; }
+
+        public bool EnableTwitter { get; set; }
+
+        public string Sitename { get; set; }
+
+        public string TwitterCard { get; set; }
+
+        public string FacebookAppId { get; set; }
+
+        #endregion Public Properties
+
+        #region Protected Methods
+
+        protected override void OnPreRender(EventArgs e)
+        {
+            base.OnPreRender(e);
+
+            AddFacebookMetaTags();
+            AddTwitterMetaTags();
+        }
+
+        private void AddFacebookMetaTags()
+        {
+            if (!EnableFacebook)
+                return;
+
+            AddSingleFacebookMetaTag("og:title", PortalSettings.ActiveTab.Title);
+            AddSingleFacebookMetaTag("og:description", PortalSettings.ActiveTab.Description);
+            AddSingleFacebookMetaTag("og:url", PortalSettings.ActiveTab.FullUrl);
+
+            if (!string.IsNullOrWhiteSpace(Sitename))
+                AddSingleFacebookMetaTag("og:site_name", Sitename);
+
+            if (!string.IsNullOrWhiteSpace(FacebookAppId))
+                AddSingleFacebookMetaTag("fb:app_id", FacebookAppId);
+        }
+
+        /// <summary>
+        /// Adds a single Metatag at the end of the html -> head section
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="content"></param>
+        private void AddMetaTag(
+            string name,
+            string content)
+        {
+            Page.Header.Controls.Add(new HtmlMeta()
+            {
+                Name = name,
+                Content = content
+            });
+        }
+
+        /// <summary>
+        /// Adds a single Metatag at the end of the html -> head section
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="content"></param>
+        private void AddSingleFacebookMetaTag(
+            string name,
+            string content)
+        {
+            var metaTag = new HtmlMeta();
+            metaTag.Attributes.Add("property", name);
+            metaTag.Content = content;
+
+            Page.Header.Controls.Add(metaTag);
+        }
+
+        private void AddTwitterMetaTags()
+        {
+            if (!EnableTwitter)
+                return;
+
+            if (!string.IsNullOrWhiteSpace(TwitterCard))
+                AddMetaTag("twitter:card", TwitterCard);
+
+            AddMetaTag("twitter:url", PortalSettings.ActiveTab.FullUrl);
+            AddMetaTag("twitter:title", PortalSettings.ActiveTab.Title);
+            AddMetaTag("twitter:description", PortalSettings.ActiveTab.Description);
+        }
+
+        #endregion Protected Methods
+    }
+}
